30f5b20f5de0c8d701edbcc382dc09d9acf2f0ce
GarPav123 <garimellapavanteja123@gmail.com>
2023-11-11 20:26:52 +0530

    testimonals and portfolio: coming soon fix

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="font-abc mt-[3%] text-5xl flex justify-center">
+      Coming Soon.....
+    </div>
+  )
+}
+
+export default page
