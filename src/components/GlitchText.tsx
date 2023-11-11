7e036ad8f2fb281820f28442c1e382e96f28e367
GarPav123 <garimellapavanteja123@gmail.com>
2023-11-11 10:35:57 +0530

    glitch text fix

--- src/components/GlitchText.tsx
+++ src/components/GlitchText.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import "./Glitch.css";
 
 interface GlitchTextProps {
@@ -6,50 +6,60 @@ interface GlitchTextProps {
   uniqueKey: string; // Add a unique key prop
 }
 
+// ... existing imports ...
+
+// ... existing imports ...
+
 function GlitchTextNoMargin({ displayText, uniqueKey }: GlitchTextProps) {
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let interval: NodeJS.Timeout | null = null;
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handleMouseOver = (event: MouseEvent) => {
+  const handleMouseOver = (event: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
+    const target = event.currentTarget;
+    if (!target) return;
+
     let iteration = 0;
 
-    clearInterval(interval as NodeJS.Timeout);
+    clearInterval(intervalRef.current as NodeJS.Timeout);
 
-    interval = setInterval(() => {
-      event.target.textContent = event.target.textContent
+    intervalRef.current = setInterval(() => {
+      const currentText = target.textContent;
+      if (!currentText) return;
+
+      target.textContent = currentText
         .split("")
         .map((letter, index) => {
           if (index < iteration) {
-            return event.target.dataset.value[index];
+            return target.dataset.value?.[index] || "";
           }
 
           return letters[Math.floor(Math.random() * 26)];
         })
         .join("");
 
-      if (iteration >= event.target.dataset.value.length) {
-        clearInterval(interval as NodeJS.Timeout);
+      if (iteration >= (target.dataset.value?.length || 0)) {
+        clearInterval(intervalRef.current as NodeJS.Timeout);
       }
 
-      iteration += 1 / 3; //control the number of times it cycles
-    }, 30); //control the time it takes per letter
+      iteration += 1 / 3; // control the number of times it cycles
+    }, 30); // control the time it takes per letter
   };
 
   useEffect(() => {
-    const divElement = document.querySelector(`#${uniqueKey}`); // Use the uniqueKey as an ID selector
+    const divElement = document.getElementById(uniqueKey);
     if (divElement) {
-      divElement.addEventListener("mouseover", handleMouseOver);
+      divElement.addEventListener("mouseover", handleMouseOver as unknown as EventListener);
 
       return () => {
-        divElement.removeEventListener("mouseover", handleMouseOver);
-        clearInterval(interval as NodeJS.Timeout);
+        divElement.removeEventListener("mouseover", handleMouseOver as unknown as EventListener);
+        clearInterval(intervalRef.current as NodeJS.Timeout);
       };
     }
   }, [uniqueKey]);
 
   return (
     <div>
-      <h1 id={uniqueKey} data-value={displayText} className=' font-abc '>
+      <h1 id={uniqueKey} data-value={displayText} className='font-abc'>
         {displayText}
       </h1>
     </div>
