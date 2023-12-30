061860a0452ab7cb00adb26667d150a737a8c81b
GarPav123 <garimellapavanteja123@gmail.com>
2023-12-30 10:41:15 +0530

    destination fix:api

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
 // next.config.ts
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: '/src/app/api/:path*', // Adjust the path accordingly
-        },
-      ];
-    },
-  };
-  
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*', // Change this back to the original path
+      },
+    ];
+  },
+};
