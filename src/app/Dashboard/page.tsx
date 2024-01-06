4f0f7b6aacf470605f30d703ae86da299d466d51
GarPav123 <garimellapavanteja123@gmail.com>
2024-01-06 21:43:56 +0530

    clerk:fix

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,20 +1,20 @@
-"use client";
-import React from "react";
-import { UserButton, useUser } from "@clerk/nextjs";
+
+
+
 
 export default function DashboardPage() {
-  const { user, isLoaded } = useUser();
+ 
   return (
     <div>
-      {isLoaded && user && (
+
         <>
           <div id='adminMainPage' className="w-full h-screen bg-white flex flex-row overflow-hidden">
             <div className="flex justify-center items-center text-black w-[80%]">Main text</div>
-            <div className="lg:pl-[30%] lg:pr-5 lg:pt-5 text-black "><UserButton afterSignOutUrl="/"></UserButton></div>
+            <div className="lg:pl-[30%] lg:pr-5 lg:pt-5 text-black ">Login Button</div>
 
           </div>
         </>
-      )}
+      
     </div>
   );
 }
