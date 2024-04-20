02b5448076623cc48c345c17e4769b9aec05bb07
GarPav123 <garimellapavanteja123@gmail.com>
2024-04-20 12:07:07 +0530

    next fix

--- next.config.js
+++ next.config.js
@@ -9,10 +9,3 @@ module.exports = {
     ];
   },
 };
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
